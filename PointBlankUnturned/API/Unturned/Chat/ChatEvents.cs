091f4106985b982175e3030c64484860c0ddf7be
AtiLion <thegrimpunisher1@gmail.com>
2017-07-17 10:40:17 +0200

    Started splitting

---
+++ PointBlankUnturned/API/Unturned/Chat/ChatEvents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Permissions;
+using PointBlank.Framework.Permissions.Ring;
+using PointBlank.API.Unturned.Player;
+
+namespace PointBlank.API.Unturned.Chat
+{
+    /// <summary>
+    /// Contains events for the chat
+    /// </summary>
+    public static class ChatEvents
+    {
+        #region Handlers
+        public delegate void ChattedHandler(ref UnturnedPlayer player, ref byte mode, ref string text, ref bool cancel);
+        #endregion
+
+        #region Events
+        public static event ChattedHandler OnChatted;
+        #endregion
+
+        #region Functions
+        internal static void RunChatted(ref UnturnedPlayer player, ref byte mode, ref string text, ref bool cancel) => OnChatted?.Invoke(ref player, ref mode, ref text, ref cancel);
+
+        #endregion
+    }
+}
---
+++ PointBlankUnturned/Loader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SDG.Framework.Modules;
+using SDG.Unturned;
+using PointBlank.API;
+
+namespace PointBlank
+{
+    internal class Loader : IModuleNexus
+    {
+        #region Properties
+        public static Loader Instance;
+        
+        #endregion
+
+        #region Nexus Interface
+        public void initialize()
+        {
+            if ((!Provider.isServer && !Dedicator.isDedicated))
+                return;
+
+            // Set the variables
+            Instance = this;
+
+            // Run code
+            Dedicator.commandWindow.title = PointBlankInfo.Name + " v" + PointBlankInfo.Version;
+        }
+
+        public void shutdown()
+        {
+            if ((!Provider.isServer && !Dedicator.isDedicated))
+                return;
+
+            // Set the variables
+            Instance = null;
+        }
+        #endregion
+    }
+}
