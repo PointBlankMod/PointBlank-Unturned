d848c7d225cfa2ea5841316a27622a341f1ec7b7
AtiLion <thegrimpunisher1@gmail.com>
2017-07-20 14:41:01 +0200

    Added dynamic page changing

--- PointBlank Launcher/js/NavigationBar.js
+++ PointBlank Launcher/js/NavigationBar.js
@@ -1,7 +1,15 @@
+const fs = require("fs");
+
 function openAppNav(id) {
   document.getElementById(id).style.width = "250px";
 }
 
 function closeAppNav(id) {
   document.getElementById(id).style.width = "0";
+}
+
+function loadPage(name) {
+  var data = fs.readFileSync("pages/" + name + ".html");
+
+  document.getElementById("Replacable").innerHTML = data.toString();
 }
